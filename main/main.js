71a7267e72764879750e583e5bd7596e1649ef07
ldliang93 <ldliang93@163.com>
2018-04-24 21:32:45 +0800

    2018 4 24

--- main/main.js
+++ main/main.js
@@ -1,54 +1,45 @@
-module.exports = function main() {
-    var items = loadAllItems();
-    const promotions = loadPromotions();
-    var selectedItems = selectionString2Obj(selectedItems);
-    const cartItems = selectedIds2CartItems(selectedItems);
+const datbase = require('../main/datbase.js');
+module.exports = function printInventory(inputs,allItems) {
+    let barcodes = getBarcodeItems(inputs);
+    let cartItems = getCartItems(barcodes, allItems);
+    let bill =  getBillList(cartItems);
+    return bill;
 };
 
-function selectionString2Obj(selectedItems){
-  var seleItems = [];
-  let temp;
-  var count = 0;
-  temp = selectedItems[0];
-    selectedItems.map((item) => {
-      if (temp === selectedItems[i]) {
-          count++;
-      }
-      if ((temp != selectedItems[i]) || (i === selectedItems.length - 1)) {
-              seleItems.push({
-                  key: temp,
-                  count: count
-              });
-              temp = selectedItems[i];
-              var count = 1;
-      }
-      if (selectedItems[i].length > 10) {
-              temp = selectedItems[i].substring(0, 10);
-              count = selectedItems[i].substring(11);
-              count = parseInt(count);
-              seleItems.push({
-                  key: temp,
-                  count: count
-              })
-          }
-    });
-    return seleItems = [];
+function getBarcodeItems(inputs){
+  var barcodes = {};
+  inputs.map(input => {
+    if(barcodes[input]){
+      barcodes[input]++;
+    }else if (input.indexOf('-') !== -1) {
+      barcodes[input.split("-")[0]] = Number(input.split("-")[1]);
+    }else{
+      barcodes[input] = 1;
+    }
+  });
+  return barcodes;
 }
 
-function selectedIds2CartItems(selectedItems){
-  let cartItems = [];
-  selectedItems.map((seleInfo) => {
-    const barcode = seleInfo.substring(0,10);
-    const item = getItemById(barcode);
-    let cartItem = {
-      name : item.name,
-      number : number,
-      unit : item.unit,
-      price : item.price,
-      totalPrice : item.price * number,
-      barcode : barcode
-    }
-    cartItems.push(cartItem)
-  });
+function getCartItems(barcodes, allItems){
+  let cartItems = allItems.filter(item => barcodes[item.barcode] ? item.num = Number(barcodes[item.barcode]) : "");
+  cartItems.filter(item => item.prom = Math.floor(barcodes[item.barcode] /3 ));
   return cartItems;
+}
+
+function getBillList(cartItems){
+  let billList = '***<没钱赚商店>购物清单***';
+  let delimiter = '----------------------' + '\n' + '挥泪赠送商品：';
+  let totalPrice = 0,save = 0;
+  cartItems.forEach(item => {
+      let itemTotalPrice = (item.num - item.prom) * item.price;
+      totalPrice += itemTotalPrice;
+      billList += '\n' + '名称：' + item.name + '，数量：' + item.num + item.unit + '，单价：' + item.price.toFixed(2) + '(元)，小计：' + itemTotalPrice.toFixed(2) + '(元)';
+
+      if (item.prom > 0) {
+          delimiter += '\n' + '名称：' + item.name + '，数量：' + item.prom + item.unit;
+          save += item.prom * item.price;
+      }
+  });
+  billList += '\n' + delimiter + '\n' + '----------------------' + '\n' + '总计：' + totalPrice.toFixed(2) + '(元)' + '\n' + '节省：' + save.toFixed(2) + '(元)' + '\n' + '**********************';
+  console.log(billList);
 }
